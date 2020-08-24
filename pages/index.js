189ef0897f0aa06ee474b5261de443de07f49b04
jedondfds <jekabs.donis@gmail.com>
2020-08-24 20:28:08 +0200

    test

--- pages/index.js
+++ pages/index.js
@@ -4,13 +4,16 @@ import styles from "../styles/Home.module.css";
 export default function Home() {
   const [isTrue, setIsTrue] = React.useState(false);
 
+  React.useEffect(() => {
+    setIsTrue(!isTrue);
+  }, [isTrue]);
+
   return (
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      {isTrue ? console.log(isTrue) : "weird"}
       <footer className={styles.footer}>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
